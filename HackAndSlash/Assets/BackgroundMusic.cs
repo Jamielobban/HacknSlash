78999b7065636e8d3a50f3977105530d7c1a73bc
Jamie <jamiealexander.lobban@enti.cat>
2023-11-26 22:31:27 +0100

    fondo

---
+++ HackAndSlash/Assets/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private AudioSource musicSource;
+    public AudioClip audiocLip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        musicSource.loop = true;
+        musicSource.clip = audiocLip;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
