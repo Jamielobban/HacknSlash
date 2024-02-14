1f325b23c384d059250b09825f2ea069ebc0f40f
Marc Calvet Palao <marc.calvet@enti.cat>
2024-02-14 13:51:02 +0100

    Fent veus

---
+++ HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/BlackCyborgSoldier.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackCyborgSoldier : Interactuable
+{
+    [SerializeField] string[] dialogues;
+    [SerializeField] SimpleRTVoiceExample voice;
+    int currentDialogue = 0; 
+
+    protected override void TriggerAction()
+    {
+        voice.Speak(dialogues[currentDialogue]);
+    }
+    
+}
