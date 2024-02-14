8151650670e15bc9089052e9e47709d4b0968246
Marc Calvet Palao <marc.calvet@enti.cat>
2024-02-14 17:12:59 +0100

    fixes

--- HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/BlackCyborgSoldier.cs
+++ HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/BlackCyborgSoldier.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackCyborgSoldier : Interactuable
+public class BlackCyborgSoldier : Interactive
 {
     [SerializeField] string[] dialogues;
     [SerializeField] SimpleRTVoiceExample voice;
