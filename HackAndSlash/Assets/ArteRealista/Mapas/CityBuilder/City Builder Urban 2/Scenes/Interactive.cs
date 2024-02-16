d2152742ed7355afa5c822d79bc8193d63da259e
Marc Calvet Palao <marc.calvet@enti.cat>
2024-02-16 20:06:12 +0100

    Canvi interactable

--- HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/Interactive.cs
+++ HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/Interactive.cs
@@ -3,40 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interactive : MonoBehaviour
+public class Interactive : MonoBehaviour
 {
     [SerializeField] Material triggerMat, normalMat;
-    bool playerInside = false, canTriggerAgain = true;
-    Collider col;
-
-    private void OnEnable()
-    {
-        col = GetComponent<Collider>();
-        int playerLayer = LayerMask.NameToLayer("Player");
-        LayerMask allLayersExceptPlayer = ~(1 << playerLayer);
-        col.excludeLayers = allLayersExceptPlayer;
-        col.includeLayers = (1 << playerLayer);
-    }
-
-    
-
-    private void Update()
-    {
-        if (canTriggerAgain && playerInside && Input.GetKeyDown(KeyCode.H))
-            TriggerAction();
-    }
-
-    private void OnTriggerEnter(Collider other)
+    protected bool canInteract = true;
+    public void ShowObjectInRange()
     {
         GetComponentInChildren<Renderer>().material = triggerMat;
-        playerInside = true;
     }
 
-    private void OnTriggerExit(Collider other)
+    public void HideObjectInRange()
     {
         GetComponentInChildren<Renderer>().material = normalMat;
-        playerInside = false;
     }
 
-    protected abstract void TriggerAction();
 }
