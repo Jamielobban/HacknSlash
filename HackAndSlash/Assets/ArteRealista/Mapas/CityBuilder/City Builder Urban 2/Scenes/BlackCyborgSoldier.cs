ca8f430a3aef83cca64acf895ef967ebfbb18d22
Marc Calvet Palao <marc.calvet@enti.cat>
2024-02-16 12:04:21 +0100

    gitflow-feature-stash: acabantcity

--- HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/BlackCyborgSoldier.cs
+++ HackAndSlash/Assets/ArteRealista/Mapas/CityBuilder/City Builder Urban 2/Scenes/BlackCyborgSoldier.cs
@@ -2,24 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackCyborgSoldier : MonoBehaviour, IInteractable
+public class BlackCyborgSoldier :  Interactive
 {
     [SerializeField] string[] dialogues;
     [SerializeField] SimpleRTVoiceExample voice;
     int currentDialogue = 0;
-    [SerializeField] Material triggerMat, normalMat;
     
-    public void Interact()
+    protected override void TriggerAction()
     {
         voice.Speak(dialogues[currentDialogue]);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        GetComponentInChildren<Renderer>().material = triggerMat;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        GetComponentInChildren<Renderer>().material = normalMat;
-    }
+   
 }
