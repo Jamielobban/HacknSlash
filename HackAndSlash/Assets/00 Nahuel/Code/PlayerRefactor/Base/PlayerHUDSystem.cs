25db344d8f599ab5c28cdeeee6779e0753fbc7f1
NahuelAparicio <nahuel.aparicio@enti.cat>
2024-02-24 20:33:04 +0100

    push de mierda

--- HackAndSlash/Assets/00 Nahuel/Code/PlayerRefactor/Base/PlayerHUDSystem.cs
+++ HackAndSlash/Assets/00 Nahuel/Code/PlayerRefactor/Base/PlayerHUDSystem.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHUDSystem : MonoBehaviour
 {
-    public Image healthBar;
-
+    public Image progressBar;
     public void UpdateHealthBar(float currentHealth, float maxHealth)
     {
-        healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHealth, 0, 1);
+        int propId = Shader.PropertyToID("_ProgressBar");
+        progressBar.material.SetFloat(propId, Mathf.Clamp(currentHealth / maxHealth, 0, 1));
     }
 }
