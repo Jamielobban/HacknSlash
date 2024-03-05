31d46e07cda95c2ef4a0650fcea0bc85ff7daf2d
NahuelAparicio <nahuel.aparicio@enti.cat>
2024-03-05 12:52:41 +0100

    Enemy Tree cosas

--- HackAndSlash/Assets/00 Nahuel/Code/Enemies/Base/BaseStates/HitState.cs
+++ HackAndSlash/Assets/00 Nahuel/Code/Enemies/Base/BaseStates/HitState.cs
@@ -18,31 +18,36 @@ public class HitState : EnemyState
         _timer = 0;
 
         this.Wait(_enemy.animations.Animator.GetCurrentAnimatorClipInfo(0).Length, () =>
-        { 
-            //if (!enemy.movements.InRangeToChase())
-            //{
-            //    _enemy.movements.ResetHit();
-
-            //    enemy.events.Idle();
-            //}
-            //else
-            //{
-                _enemy.movements.ResetHit();
-
-                enemy.events.Following();
-            //}
+        {
+            _enemy.movements.ResetHit();
+            enemy.events.Following();
         });
+        _enemy.movements.GetRigidBody().isKinematic = false;
     }
+    RaycastHit _hit;
+    Vector3 _targetPostion;
+    
     public override void UpdateState(Enemy enemy)
     {
         _force = _enemy.currentHitEffect.animationCurve.Evaluate(Time.time - _timerCurve);
-        Vector3 dir = (transform.position - _enemy._player.transform.position);
-        _enemy.movements.GetRigidBody().AddForce(dir * _force * 5000f * Time.deltaTime, ForceMode.Force);
+        Vector3 playerPosition = _enemy._player.transform.position;
+        Vector3 dir = (transform.position - playerPosition);
+        dir.y = 0;
+        _enemy.movements.GetRigidBody().AddForce(dir.normalized * _force * 5000f * Time.deltaTime, ForceMode.Force);
+        if(Physics.Raycast(transform.position, Vector3.down, out _hit, 50f, LayerMask.GetMask("Ground")))
+        {
+            _targetPostion = transform.position;
+            _targetPostion.y = _hit.point.y;
+            transform.position = Vector3.Lerp(transform.position, _targetPostion, Time.deltaTime / 0.1f);
+            transform.position = _targetPostion;
+        }
     }
 
     public override void ExitState(Enemy enemy)
     {
         base.ExitState(enemy);
+        _enemy.movements.GetRigidBody().isKinematic = true;
+
         _timer = 0;
         _timerCurve = 0;
     }
