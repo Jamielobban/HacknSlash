0d2ac66a5754bc0fee54f7cb106caa152335eac9
Marc Calvet Palao <marc.calvet@enti.cat>
2024-04-28 19:54:13 +0200

    move fixed

--- HackAndSlash/Assets/Art/Mapas/Cinematica/HologramCorridors/CamMovement.cs
+++ HackAndSlash/Assets/Art/Mapas/Cinematica/HologramCorridors/CamMovement.cs
@@ -9,14 +9,14 @@ public class CamMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Tween moveTween = this.transform.DOMoveX(3.79f, 14).SetEase(Ease.InOutSine);
+        Tween moveTween = this.transform.DOMoveX(15.46f, 20).SetEase(Ease.InOutSine); //3.79f
         Tween moveTween2 = this.transform.DOMoveX(15.46f, 6).SetEase(Ease.InOutSine);
         Tween rotLeftTween = this.transform.DORotate(new Vector3(0,45,0), 4).SetEase(Ease.InOutSine);
         Tween rotRightTween = this.transform.DORotate(new Vector3(0, 135, 0), 4).SetEase(Ease.InOutSine);
         Tween rotStraightTween = this.transform.DORotate(new Vector3(0, 90, 0), 4).SetEase(Ease.InOutSine);
 
         sequence = DOTween.Sequence();
-        sequence.Append(moveTween).Append(rotLeftTween).Append(rotRightTween).Append(rotStraightTween).Append(moveTween2);
+        sequence.Append(moveTween)/*.Append(rotLeftTween).Append(rotRightTween).Append(rotStraightTween).Append(moveTween2)*/;
     }
 
     private void OnTriggerEnter(Collider other)
