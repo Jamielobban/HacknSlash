1bee7148a20169bbfba6dfc6c8ae068068dd9abf
Marc Calvet Palao <marc.calvet@enti.cat>
2024-04-28 18:39:26 +0200

    Mov camera

--- HackAndSlash/Assets/Art/Mapas/Cinematica/HologramCorridors/CamMovement.cs
+++ HackAndSlash/Assets/Art/Mapas/Cinematica/HologramCorridors/CamMovement.cs
@@ -5,16 +5,18 @@ using DG.Tweening;
 
 public class CamMovement : MonoBehaviour
 {
+    Sequence sequence;
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.DOMoveX(3.79f, 14).SetEase(Ease.InOutSine);
-    }
+        Tween moveTween = this.transform.DOMoveX(3.79f, 14).SetEase(Ease.InOutSine);
+        Tween moveTween2 = this.transform.DOMoveX(15.46f, 6).SetEase(Ease.InOutSine);
+        Tween rotLeftTween = this.transform.DORotate(new Vector3(0,45,0), 4).SetEase(Ease.InOutSine);
+        Tween rotRightTween = this.transform.DORotate(new Vector3(0, 135, 0), 4).SetEase(Ease.InOutSine);
+        Tween rotStraightTween = this.transform.DORotate(new Vector3(0, 90, 0), 4).SetEase(Ease.InOutSine);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        sequence = DOTween.Sequence();
+        sequence.Append(moveTween).Append(rotLeftTween).Append(rotRightTween).Append(rotStraightTween).Append(moveTween2);
     }
 
     private void OnTriggerEnter(Collider other)
