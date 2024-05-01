925b366053c6ea4c8930e1273c2690c7cb352c85
Marc Calvet Palao <marc.calvet@enti.cat>
2024-05-01 23:51:11 +0200

    gitflow-feature-stash: bugsTutoCOmbos
    hfghg

--- HackAndSlash/Assets/Scripts/Tutorials/DoorTrigger.cs
+++ HackAndSlash/Assets/Scripts/Tutorials/DoorTrigger.cs
@@ -25,14 +25,14 @@ public class DoorTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        pc.playerAnim.CrossFadeInFixedTime("Walk", 0.2f);
-        pc.transform.GetChild(0).LookAt(new Vector3(pc.transform.position.x, pc.transform.position.y, 94.7f));
-        pc.transform.DOLocalMoveZ(94.7f, 4);
-        pc.enabled = false;
-        rightDoor.DOLocalMoveY(0.506f, 1);
-        leftDoor.DOLocalMoveY(0.598f, 1);
-        tm.GetFade.FadeOut(2.5f);
-        tm.MuteInSeconds(2.5f);
-        tm.EndTutorial();
+        //pc.playerAnim.CrossFadeInFixedTime("Walk", 0.2f);
+        //pc.transform.GetChild(0).LookAt(new Vector3(pc.transform.position.x, pc.transform.position.y, 94.7f));
+        //pc.transform.DOLocalMoveZ(94.7f, 4);
+        //pc.enabled = false;
+        //rightDoor.DOLocalMoveY(0.506f, 1);
+        //leftDoor.DOLocalMoveY(0.598f, 1);
+        //tm.GetFade.FadeOut(2.5f);
+        //tm.MuteInSeconds(2.5f);
+        //tm.EndTutorial();
     }
 }
