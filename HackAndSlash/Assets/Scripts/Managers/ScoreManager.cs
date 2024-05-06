89cfa7da65237fe412b2adbff605b704b760c624
NahuelAparicio <nahuel.aparicio@enti.cat>
2024-05-06 12:42:08 +0200

    Score Management multiplier

--- HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
+++ HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
@@ -4,25 +4,57 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI currentScoreText;
+    public TextMeshProUGUI multiplierText;
     public float timeToStartResetScore;
-    public float timeToRemoveScore;
     public float removeScore = 10;
-    public int maxScore;
 
+    private int maxScore = 100;
     private float _enemiesScore = 0;
     private float lastTimeScored;
-    private float _curretTime;
+    private PlayerControl _player;
+
+    private void Awake()
+    {
+        _player = GameManager.Instance.Player;
+    }
 
     void Update()
     {
         if ((Time.time - lastTimeScored) >= timeToStartResetScore)
         {
-            _curretTime += Time.deltaTime;
-            if(_curretTime >= timeToRemoveScore)
-            {
-                AddEnemyScore(-10);
-                _curretTime = 0;
-            }
+            _enemiesScore -= removeScore * Time.deltaTime;
+
+            _enemiesScore = Mathf.Clamp(_enemiesScore, 0, maxScore);
+
+            UpdateScore();
+        }
+
+        if(_enemiesScore < 25 && _player.multiplierCombos != 1)
+        {
+            multiplierText.text = "";
+            _player.multiplierCombos = 1;
+
+        }
+        else if(_enemiesScore >= 25 && _enemiesScore < 50 && _player.multiplierCombos != 1.25f)
+        {
+            multiplierText.text = "x1.25";
+            _player.multiplierCombos = 1.25f;
+
+        }
+        else if(_enemiesScore >= 50 && _enemiesScore < 75 && _player.multiplierCombos != 1.5f)
+        {
+            multiplierText.text = "x1.5";
+            _player.multiplierCombos = 1.5f;
+        }
+        else if(_enemiesScore >= 75 && _enemiesScore < 100 && _player.multiplierCombos != 1.75f)
+        {
+            multiplierText.text = "x1.75";
+            _player.multiplierCombos = 1.75f;
+        }
+        else if(_enemiesScore >= 100 && _player.multiplierCombos != 2f)
+        {
+            multiplierText.text = "x2";
+            GameManager.Instance.Player.multiplierCombos = 2;
         }
     }
 
@@ -45,6 +77,9 @@ public class ScoreManager : MonoBehaviour
         _enemiesScore += val;
         if (_enemiesScore <= 0)
             _enemiesScore = 0;
+
+        if (_enemiesScore >= maxScore)
+            _enemiesScore = maxScore;
         UpdateScore();
     }
 
