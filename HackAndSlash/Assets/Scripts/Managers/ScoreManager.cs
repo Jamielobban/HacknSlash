c6cb51e9654b9fcc60a3d519ee775279cc526a78
Daniel Sierra Moreno <daniel.sierra@enti.cat>
2024-05-06 14:35:57 +0200

    mapa

--- HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
+++ HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
@@ -13,9 +13,16 @@ public class ScoreManager : MonoBehaviour
     private float lastTimeScored;
     private PlayerControl _player;
 
+    public GameObject[] efectX15;
+    public GameObject[] efectX2;
+    public ObjectShake shake;
     private void Awake()
     {
         _player = GameManager.Instance.Player;
+        efectX15[0].SetActive(false);
+        efectX15[1].SetActive(false);
+        efectX2[0].SetActive(false);
+        efectX2[1].SetActive(false);
     }
 
     void Update()
@@ -33,26 +40,43 @@ public class ScoreManager : MonoBehaviour
         {
             multiplierText.text = "";
             _player.multiplierCombos = 1;
-
+            efectX15[0].SetActive(false);
+            efectX15[1].SetActive(false);
         }
         else if(_enemiesScore >= 25 && _enemiesScore < 50 && _player.multiplierCombos != 1.25f)
         {
             multiplierText.text = "x1.25";
             _player.multiplierCombos = 1.25f;
-
+            efectX15[0].SetActive(true);
+            efectX15[1].SetActive(true);
+            shake.shakeMagnitude = 1;
         }
         else if(_enemiesScore >= 50 && _enemiesScore < 75 && _player.multiplierCombos != 1.5f)
         {
+            efectX2[0].SetActive(false);
+            efectX2[1].SetActive(false);
+            efectX15[0].SetActive(true);
+            efectX15[1].SetActive(true);
+            shake.shakeMagnitude = 2;
+
             multiplierText.text = "x1.5";
             _player.multiplierCombos = 1.5f;
         }
         else if(_enemiesScore >= 75 && _enemiesScore < 100 && _player.multiplierCombos != 1.75f)
         {
+            efectX15[0].SetActive(false);
+            efectX15[1].SetActive(false);
+            efectX2[0].SetActive(true);
+            efectX2[1].SetActive(true);
+            shake.shakeMagnitude = 4;
+
             multiplierText.text = "x1.75";
             _player.multiplierCombos = 1.75f;
         }
         else if(_enemiesScore >= 100 && _player.multiplierCombos != 2f)
         {
+            shake.shakeMagnitude = 6;
+
             multiplierText.text = "x2";
             GameManager.Instance.Player.multiplierCombos = 2;
         }
