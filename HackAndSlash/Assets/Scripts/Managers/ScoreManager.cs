a36e2ceef1deb1c93b2323975975380953528926
NahuelAparicio <nahuel.aparicio@enti.cat>
2024-05-06 11:28:21 +0200

    Delete City + Score Bar

--- HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
+++ HackAndSlash/Assets/Scripts/Managers/ScoreManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI currentScoreText;
-    private float _enemiesScore = 0;
-
     public float timeToStartResetScore;
+    public float timeToRemoveScore;
+    public float removeScore = 10;
+
+    private float _enemiesScore = 0;
+    private float lastTimeScored;
+    private float _curretTime;
 
     void Update()
     {
-        
+        if ((Time.time - lastTimeScored) >= timeToStartResetScore)
+        {
+            _curretTime += Time.deltaTime;
+            if(_curretTime >= timeToRemoveScore)
+            {
+                AddEnemyScore(-10);
+                _curretTime = 0;
+            }
+        }
     }
 
     public bool IsMaxScore() => _enemiesScore >= GameManager.Instance.Player.hud.maxScore;
@@ -28,6 +40,7 @@ public class ScoreManager : MonoBehaviour
 
     public void AddEnemyScore(float val)
     {
+        lastTimeScored = Time.time;
         _enemiesScore += val;
         UpdateScore();
     }
